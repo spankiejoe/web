1e600f03780085d9cf77c80ccf1f62952f5ce36e
Ralf Biedert <rb@xr.io>
2014-03-22 10:18:32 +0100

    Added videos to showcase.

--- examples/index.php
+++ examples/index.php
@@ -14,14 +14,24 @@
 
 
         <br/>
-		Got inspired? Have something cool to show? <a href="info@gaze.io">Let us know!</a></p>
+		Got inspired? Have something cool to show? <a href="info@gaze.io">Let us know</a>, or
+        clone <a href="https://github.com/gazeio/showcase">our showcase project</a>.</p>
 
 
         <h2>Videos</h2>
-        <p>...</p>
+        Below you can find a number of older videos that have been built 
+        upon gaze.io's ancestor library the Text 2.0 framework. While in the underlying architecture
+        many things have changed since then, they might still be inspirational. Have fun :)
 
-        blah
+        <h5>Text 2.0</h5>        
+        <iframe width="560" height="315" src="//www.youtube.com/embed/8QocWsWd7fc" frameborder="0" allowfullscreen></iframe>       
 
+
+        <h5>Webkit 3D + Eye Tracking</h5>        
+        <iframe width="560" height="315" src="//www.youtube.com/embed/RmBI7WYveYo" frameborder="0" allowfullscreen></iframe>
+
+        <h5>eyePad</h5>        
+        <iframe width="420" height="315" src="//www.youtube.com/embed/7UZhB2ad8GE" frameborder="0" allowfullscreen></iframe>
     </div>
 
 <div>
