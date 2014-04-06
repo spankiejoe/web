1029308f6b78f9f9c3f0ccdad144e7b25917a154
Ralf Biedert <rb@xr.io>
2014-04-06 15:23:00 +0200

    Cleaned up showcase generation.

--- examples/index.php
+++ examples/index.php
@@ -23,14 +23,10 @@
         <br/>
 
 
+        <!-- Will be filled by script below -->
+        <p id="showbar"></p>
 
-        <p id="showbar">
-            <a href="/examples/fft/"><img class="demo" id="demofft" src="/examples/fft.png"/></a>
-            <a href="/examples/glimpse/"><img class="demo" id="demoglimpse" src="/examples/glimpse.jpg"/></a>
-            <a href="/examples/heatmap/"><img class="demo" id="demoheatmap" src="/examples/heatmap.jpg"/></a>
-            <a href="/examples/reading/"><img class="demo" id="demoreading" src="/examples/reading.jpg"/></a>
-            <center style="font-size:75%;">Click to launch demo, hover for details.</center>
-        </p>
+        <center style="font-size:75%;">Click to launch demo, hover for details.</center>
 
         <br/><br/>
 
@@ -75,131 +71,16 @@
 
 
 <script>
-    var demos = {
-        demofft: {
-            title: "Gaze FFT",
-            description: "In the FFT demo we visualize raw gaze data in a special form. \
-            We <a href='http://nayuki.eigenstate.org/page/free-small-fft-in-multiple-languages'> \
-            transform the data</a> from the time into the frequency domain, so that \
-            the individual vertical and horizontal sine-components become visible. <br/><br/> \
-            \
-            Apart from the fact that it looks nice, the spectral analysis of gaze data \
-            can give you a better understanding what your precision and accuracy error is \
-            actually composed of. <br/><br/>\
-            \
-            The full source of this demo can be found in the \
-            <a href='https://github.com/gazeio/showcase'>GitHub showcase repository</a>.\
-            ",
-            code: "&lt;script src='gaze.js'>&lt;/script> \
-                \n&lt;script> \
-                \n  gaze.init() \
-                \n  \
-                \n  // We tap into the raw eye tracker signal. \
-                \n  gaze.onraw(function(raw) { \
-                \n      var x = raw.left.screenX;\
-                \n      var y = raw.left.screenX;\
-                \n  \
-                \n      // And update using an external FFT library.\
-                \n      ... \
-                \n  })\
-            \n&lt;/script>\
-            "
-        },
+    var demos = { }
 
-
-        demoglimpse: {
-            title: "Glimpse",
-            description: "Inspired by <a href='http://www.spritzinc.com/'>Spritz</a> we \
-            thought their demo would be even more interesting if it were gaze aware.<br/> <br/>\
-            \
-            Similar to Spritz, text in this demo is presented word-by-word. In addition we register \
-            a gaze over / gaze out listener on the central display area and pause as long as the user \
-            is not looking. <br/><br/>\
-            \
-            The full source of this demo can be found in the \
-            <a href='https://github.com/gazeio/showcase'>GitHub showcase repository</a>.\
-            ",
-            code: "&lt;script src='gaze.js'>&lt;/script> \
-                \n&lt;script> \
-                \n  gaze.init() \
-                \n  \
-                \n  // Register a callback when gaze enters or leaves. \
-                \n  gaze.ongazeover(function(e) { \
-                \n  \
-                \n      if (e.type == 'over') { /* Start reading ... */ }\
-                \n      if (e.type == 'out') { /* Stop reading ... */ } \
-                \n  })\
-            \n&lt;/script>\
-            ",
-        },
-
-
-        demoheatmap: {
-            title: "Heat Maps",
-            description: "Everyone loves heat maps. They allow for an intuitive \
-            visualization of one or many viewers, and creating one in HTML5 thanks to the \
-            awesome <a href='http://www.patrick-wied.at/static/heatmapjs/'>heatmap.js</a> \
-            library is a piece of cake. <br/><br/>\
-            \
-            The full source of this demo can be found in the \
-            <a href='https://github.com/gazeio/showcase'>GitHub showcase repository</a>.\
-            ",
-            code: "&lt;script src='gaze.js'>&lt;/script> \
-                \n&lt;script> \
-                \n  gaze.init() \
-                \n  \
-                \n  var heatmap = ... // from heatmap.js library \
-                \n  \
-                \n  // Use smooth, filtered data for the heatmap. \
-                \n  gaze.onfiltered(function(f) { \
-                \n  \
-                \n      heatmap.store.addDataPoint(f.windowX, f.windowY, 1);\
-                \n  })\
-                \n&lt;/script>\
-            "
-        },
-        demoreading: {
-            title: "Reading / Skimming Classification",
-            description: "This demo shows a real time reading / skimming classifier. Based \
-            on your eye movement in the document the library tries to guesstimate \
-            if you are more likely reading for meaning, or if you are rather skimming the \
-            document without carefully considering it word for word. <br/><br/>\
-            \
-            Behind the curtains it shows that the core library can be dynamically \
-            extended (the classifier function is not part of the main library yet)\
-            and how this is done. \
-            <br/><br/>\
-            \
-            And in case you are interested, the underlying classifier is a (really quick and really dirty) \
-            re-implementation of: <br/><br/>\
-            <i>Biedert, R., Buscher, G., Hees, J., &amp; Dengel, A. (2012). \
-            A Robust Realtime Reading-Skimming Classifier (pp. 123–130). Presented at the \
-            Proceedings of the 2012 Symposium on Eye-Tracking Research &amp; Applications. \
-            doi:10.1145/2168556.2168575 </i>\
-            <br/><br/>\
-            \
-            The full source of this demo can be found in the \
-            <a href='https://github.com/gazeio/showcase'>GitHub showcase repository</a>.\
-            ",
-            code: "&lt;script src='gaze.js'>&lt;/script> \
-                \n&lt;script> \
-                \n  // Define your own extension. \
-                \n  gaze.extend({ \
-                \n      onreading: function(listener) { \
-                \n          // Register the listener ... \
-                \n      } \
-                \n  }, { id: 'reading'});\
-                \n  \
-                \n  // And later use it \
-                \n  gaze.onreading(function(r) { \
-                \n      // ... \
-                \n  })\
-            \n&lt;/script>\
-            "
-        },
-
+    function showcase(id, description) {
+        demos[id] = description
+        $("#showbar").append('<a href="/examples/' + id + '/"><img class="demo" id="' + id + '" src="/examples/' + id + '.jpg"/></a>')
     }
+</script>
+<script src="/examples/showcases.js"></script>
 
+<script>
     // Called to update the demo area
     function showDemo (name) {
         $("#demotitle").html(demos[name].title)
@@ -223,7 +104,7 @@
 
 
     setTimeout(function() {
-        showDemo("demofft", 1);
+        showDemo("fft", 1);
     })
 </script>
 
